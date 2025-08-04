d3f66fcdbca27eaddc6d6cd3e9df3a9abf3835b7
BalambaDumba <102482333+BalambaDumba@users.noreply.github.com>
2025-08-04 15:30:29 +0200

    runtime.py

--- runtime.py
+++ runtime.py
@@ -2,5 +2,4 @@
 
 CRYPTO_PANIC_API_KEY = 'af817ff791c26420ecceeedd3ecc542036005e66'
 TELEGRAM_BOT_TOKEN = '8247439105:AAF-2hKCxY0MXgpSupjtxYV0hk1MGU6nXt0'
-CHAT_ID = 'your_chat_id'  # Подставишь после запуска
 CHECK_INTERVAL = 300  # в секундах (5 минут)
